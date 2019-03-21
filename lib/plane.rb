f2cde267308b380da81520a4b0b034b1bacce020
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-03-21 14:42:16 +0000

    Landed method added to plane starts off not landed

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,11 @@
 require "plane"
 
 class Plane
+
+  attr_reader :landed
+
+  def initialize
+    @landed = false
+  end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,14 @@ require "plane"
 
 describe Plane do
 
+  before do
+    @plane = Plane.new
+  end
+
+  describe "#landed?" do
+    it "starts off not landed" do
+      expect(@plane.landed).to eq(false)
+    end
+  end
+
 end
