c9bf0b38ebbac3d51085623857433de19539da4d
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-03-21 13:51:48 +0000

    Removing past attempt

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,5 @@
 require_relative "plane"
 
 class Airport
-  attr_reader :planes
-  def initialize
-    @planes = []
-  end
-
-  def land(plane)
-    @planes.push(plane)
-  end
-
-  def take_off
-    @planes.pop
-  end
+  
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,22 +1,5 @@
 require "airport"
 
 describe Airport do
-    context "creates airport, plane and lands it" do
-      airport = Airport.new
-      plane = Plane.new
-      airport.land(plane)
 
-    describe '#land' do
-      it "lands a plane" do
-        expect(airport.planes).to eq([plane])
-      end
-    end
-
-    describe '#take_off' do
-      it "allows plane to take off" do
-        airport.take_off
-        expect(airport.planes).to eq([])
-      end
-    end
-  end
 end
