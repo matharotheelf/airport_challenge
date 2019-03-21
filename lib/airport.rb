c4934c2dfe11daa749d313effd7d2307377d233c
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-03-21 15:18:59 +0000

    Airport initialises empty plane list

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,11 @@
 require_relative "plane"
 
 class Airport
-  
+
+  attr_reader :plane_list
+
+  def initialize
+    @plane_list = []
+  end
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,4 +2,14 @@ require "airport"
 
 describe Airport do
 
+  before do
+    @airport = Airport.new
+  end
+
+  describe "#plane_list" do
+
+    it "starts off empty" do
+      expect(@airport.plane_list).to eq([])
+    end
+  end
 end
